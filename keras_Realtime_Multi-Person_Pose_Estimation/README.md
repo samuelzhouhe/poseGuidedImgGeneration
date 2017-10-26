ce7328b043883fc5fb5885d5b422e78d5f7acbac
SamuelHe <hezhou556@gmail.com>
2017-10-26 20:24:18 +0800

    keras pose estimation model

---
+++ keras_Realtime_Multi-Person_Pose_Estimation/README.md
@@ -0,0 +1,100 @@
+# Realtime Multi-Person Pose Estimation
+This is a keras version of [Realtime Multi-Person Pose Estimation](https://github.com/ZheC/Realtime_Multi-Person_Pose_Estimation) project  
+
+## Introduction
+Code repo for reproducing [2017 CVPR](https://arxiv.org/abs/1611.08050) paper using keras.  
+
+## Results
+
+<p align="center">
+<img src="https://github.com/michalfaber/keras_Realtime_Multi-Person_Pose_Estimation/blob/master/readme/dance.gif", width="720">
+</p>
+
+<div align="center">
+<img src="https://github.com/michalfaber/keras_Realtime_Multi-Person_Pose_Estimation/blob/master/sample_images/ski.jpg", width="300", height="300">
+&nbsp;
+<img src="https://github.com/michalfaber/keras_Realtime_Multi-Person_Pose_Estimation/blob/master/readme/result.png", width="300", height="300">
+</div>
+
+
+## Contents
+1. [Converting caffe model](#converting-caffe-model-to-keras-model)
+2. [Testing](#testing-steps)
+3. [Training](#training-steps)
+
+## Require
+1. [Keras](https://keras.io/)
+2. [Caffe - docker](https://hub.docker.com/r/bvlc/caffe/) required if you would like to convert caffe model to keras model. You 
+ don't have to compile/install caffe on your local machine.
+
+## Converting Caffe model to Keras model
+Authors of [original implementation](https://github.com/ZheC/Realtime_Multi-Person_Pose_Estimation) released already trained caffe model 
+which you can use to extract weights data.   
+
+- Download caffe model `cd model; sh get_caffe_model.sh`
+- Dump caffe layers to numpy data `cd ..; docker run -v [absolute path to your keras_Realtime_Multi-Person_Pose_Estimation folder]:/workspace -it bvlc/caffe:cpu python dump_caffe_layers.py`
+  Note that docker accepts only absolute paths so you have to set the full path to the folder containing this project.
+- Convert caffe model (from numpy data) to keras model `python caffe_to_keras.py`  
+
+## Testing steps
+- Convert caffe model to keras model or download already converted keras model https://www.dropbox.com/s/llpxd14is7gyj0z/model.h5
+- Run the notebook `demo.ipynb`.
+- `python demo_image.py --image sample_images/ski.jpg` to run the picture demo. Result will be stored in the file result.png. You can use
+any image file as an input.
+- `python demo_camera.py` to run the web demo.
+
+## Training steps
+**UPDATE 10/2017:**
+
+**-Augmented samples are fetched from the [server](https://github.com/michalfaber/rmpe_dataset_server). The network never sees the same image twice
+  which was a problem in previous approach (tool rmpe_dataset_transformer)
+  This allows you to run augmentation locally or on separate node. 
+  You can start 2 instances, one serving training set and a second one serving validation set (on different port if locally)** 
+  
+**-Experimentally I've added image normalization as in vgg paper (images should be zero-centered by mean pixel subtraction)**
+
+- Install gsutil `curl https://sdk.cloud.google.com | bash`. This is a really helpful tool for downloading large datasets. 
+- Download the data set (~25 GB) `cd dataset; sh get_dataset.sh`,
+- Download [COCO official toolbox](https://github.com/pdollar/coco) in `dataset/coco/` . 
+- `cd coco/PythonAPI; sudo python setup.py install` to install pycocotools.
+- Go to the "training" folder `cd ../../../training`.
+- Generate masks `python generate_masks.py`. Note: set the parameter "mode" in generate_masks.py (validation or training) 
+- Create intermediate dataset `python generate_hdf5.py`. This tool creates a dataset in hdf5 format. The structure of this dataset is very similar to the 
+    original lmdb dataset where a sample is represented as an array: 5 x width x height (3 channels for image, 1 channel for metedata, 1 channel for miss masks)
+    For MPI dataset there are 6 channels with additional all masks.
+    Note: set the parameters `datasets` and `val_size` in `generate_hdf5.py`
+- Download and compile the dataset server [rmpe_dataset_server](https://github.com/michalfaber/rmpe_dataset_server).
+  This server generates augmented samples on the fly. Source samples are retrieved from previously generated hdf5 dataset file.                           
+- Start training data server in the first terminal session. 
+    `./rmpe_dataset_server ../../keras_Realtime_Multi-Person_Pose_Estimation/dataset/train_dataset.h5 5555`
+- Start validation data server in a second terminal session.
+    `./rmpe_dataset_server ../../keras_Realtime_Multi-Person_Pose_Estimation/dataset/val_dataset.h5 5556`
+- Optionally you can verify the datasets `inspect_dataset.ipynb`
+- Set the correct number of samples within `python train_pose.py` - variables "train_samples = ???" and "val_samples = ???".  
+ This number is used by keras to determine how many samples are in 1 epoch.
+- Train the model in a third terminal `python train_pose.py`
+
+NOTE:
+I trained the model from scratch for 3,5 days on a single GPU 1070 but did't obtain satisfactory results.
+38 epochs is about 200000 iterations in caffe. 
+I noticed that reducing learning rate after the step 136106 (as in orginal caffe model) was probably too early
+because learning process slowed down.
+ 
+<div align="center">
+<img src="https://github.com/michalfaber/keras_Realtime_Multi-Person_Pose_Estimation/blob/master/readme/tr_results.png", width="450", height="563">
+</div>
+    
+## Related repository
+- CVPR'16, [Convolutional Pose Machines](https://github.com/shihenw/convolutional-pose-machines-release).
+- CVPR'17, [Realtime Multi-Person Pose Estimation](https://github.com/ZheC/Realtime_Multi-Person_Pose_Estimation).
+
+## Citation
+Please cite the paper in your publications if it helps your research:    
+
+    @InProceedings{cao2017realtime,
+      title = {Realtime Multi-Person 2D Pose Estimation using Part Affinity Fields},
+      author = {Zhe Cao and Tomas Simon and Shih-En Wei and Yaser Sheikh},
+      booktitle = {The IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+      year = {2017}
+      }
+	  
---
+++ keras_Realtime_Multi-Person_Pose_Estimation/training/train_pose.py
@@ -0,0 +1,182 @@
+import sys
+import os
+import pandas
+import re
+import math
+sys.path.append("..")
+from model import get_training_model
+from ds_iterator import DataIterator
+from ds_generator_client import DataGeneratorClient
+from optimizers import MultiSGD
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, CSVLogger, TensorBoard
+from keras.layers.convolutional import Conv2D
+from keras.applications.vgg19 import VGG19
+
+batch_size = 10
+base_lr = 4e-5 # 2e-5
+momentum = 0.9
+weight_decay = 5e-4
+lr_policy =  "step"
+gamma = 0.333
+stepsize = 136106 #68053   // after each stepsize iterations update learning rate: lr=lr*gamma
+max_iter = 200000 # 600000
+
+# True = start data generator client, False = use augmented dataset file (deprecated)
+use_client_gen = True
+
+WEIGHTS_BEST = "weights.best.h5"
+TRAINING_LOG = "training.csv"
+LOGS_DIR = "./logs"
+
+def get_last_epoch():
+    data = pandas.read_csv(TRAINING_LOG)
+    return max(data['epoch'].values)
+
+
+model = get_training_model(weight_decay, vgg_norm=True)
+
+from_vgg = dict()
+from_vgg['conv1_1'] = 'block1_conv1'
+from_vgg['conv1_2'] = 'block1_conv2'
+from_vgg['conv2_1'] = 'block2_conv1'
+from_vgg['conv2_2'] = 'block2_conv2'
+from_vgg['conv3_1'] = 'block3_conv1'
+from_vgg['conv3_2'] = 'block3_conv2'
+from_vgg['conv3_3'] = 'block3_conv3'
+from_vgg['conv3_4'] = 'block3_conv4'
+from_vgg['conv4_1'] = 'block4_conv1'
+from_vgg['conv4_2'] = 'block4_conv2'
+
+# load previous weights or vgg19 if this is the first run
+if os.path.exists(WEIGHTS_BEST):
+    print("Loading the best weights...")
+
+    model.load_weights(WEIGHTS_BEST)
+    last_epoch = get_last_epoch() + 1
+else:
+    print("Loading vgg19 weights...")
+
+    vgg_model = VGG19(include_top=False, weights='imagenet')
+
+    for layer in model.layers:
+        if layer.name in from_vgg:
+            vgg_layer_name = from_vgg[layer.name]
+            layer.set_weights(vgg_model.get_layer(vgg_layer_name).get_weights())
+            print("Loaded VGG19 layer: " + vgg_layer_name)
+
+    last_epoch = 0
+
+# prepare generators
+
+if use_client_gen:
+    train_client = DataGeneratorClient(port=5555, host="localhost", hwm=160, batch_size=10)
+    train_client.start()
+    train_di = train_client.gen()
+    train_samples = 52597
+
+    val_client = DataGeneratorClient(port=5556, host="localhost", hwm=160, batch_size=10)
+    val_client.start()
+    val_di = val_client.gen()
+    val_samples = 2645
+else:
+    train_di = DataIterator("../dataset/train_dataset.h5", data_shape=(3, 368, 368),
+                      mask_shape=(1, 46, 46),
+                      label_shape=(57, 46, 46),
+                      vec_num=38, heat_num=19, batch_size=batch_size, shuffle=True)
+    train_samples=train_di.N
+    val_di = DataIterator("../dataset/val_dataset.h5", data_shape=(3, 368, 368),
+                      mask_shape=(1, 46, 46),
+                      label_shape=(57, 46, 46),
+                      vec_num=38, heat_num=19, batch_size=batch_size, shuffle=True)
+    val_samples=val_di.N
+
+# setup lr multipliers for conv layers
+lr_mult=dict()
+for layer in model.layers:
+
+    if isinstance(layer, Conv2D):
+
+        # stage = 1
+        if re.match("Mconv\d_stage1.*", layer.name):
+            kernel_name = layer.weights[0].name
+            bias_name = layer.weights[1].name
+            lr_mult[kernel_name] = 1
+            lr_mult[bias_name] = 2
+
+        # stage > 1
+        elif re.match("Mconv\d_stage.*", layer.name):
+            kernel_name = layer.weights[0].name
+            bias_name = layer.weights[1].name
+            lr_mult[kernel_name] = 4
+            lr_mult[bias_name] = 8
+
+        # vgg
+        else:
+           kernel_name = layer.weights[0].name
+           bias_name = layer.weights[1].name
+           lr_mult[kernel_name] = 1
+           lr_mult[bias_name] = 2
+
+# configure loss functions
+losses = {}
+losses["weight_stage1_L1"] = "mean_squared_error"
+losses["weight_stage1_L2"] = "mean_squared_error"
+losses["weight_stage2_L1"] = "mean_squared_error"
+losses["weight_stage2_L2"] = "mean_squared_error"
+losses["weight_stage3_L1"] = "mean_squared_error"
+losses["weight_stage3_L2"] = "mean_squared_error"
+losses["weight_stage4_L1"] = "mean_squared_error"
+losses["weight_stage4_L2"] = "mean_squared_error"
+losses["weight_stage5_L1"] = "mean_squared_error"
+losses["weight_stage5_L2"] = "mean_squared_error"
+losses["weight_stage6_L1"] = "mean_squared_error"
+losses["weight_stage6_L2"] = "mean_squared_error"
+
+loss_weights = {}
+loss_weights["weight_stage1_L1"] = 1
+loss_weights["weight_stage1_L2"] = 1
+loss_weights["weight_stage2_L1"] = 1
+loss_weights["weight_stage2_L2"] = 1
+loss_weights["weight_stage3_L1"] = 1
+loss_weights["weight_stage3_L2"] = 1
+loss_weights["weight_stage4_L1"] = 1
+loss_weights["weight_stage4_L2"] = 1
+loss_weights["weight_stage5_L1"] = 1
+loss_weights["weight_stage5_L2"] = 1
+loss_weights["weight_stage6_L1"] = 1
+loss_weights["weight_stage6_L2"] = 1
+
+# learning rate schedule - equivalent of caffe lr_policy =  "step"
+iterations_per_epoch = train_samples // batch_size
+def step_decay(epoch):
+    initial_lrate = base_lr
+    steps = epoch * iterations_per_epoch
+
+    lrate = initial_lrate * math.pow(gamma, math.floor(steps/stepsize))
+
+    return lrate
+
+# configure callbacks
+lrate = LearningRateScheduler(step_decay)
+checkpoint = ModelCheckpoint(WEIGHTS_BEST, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', period=1)
+csv_logger = CSVLogger(TRAINING_LOG, append=True)
+tb = TensorBoard(log_dir=LOGS_DIR, histogram_freq=0, write_graph=True, write_images=False)
+
+callbacks_list = [lrate, checkpoint, csv_logger, tb]
+
+# sgd optimizer with lr multipliers
+multisgd = MultiSGD(lr=base_lr, momentum=momentum, decay=0.0, nesterov=False, lr_mult=lr_mult)
+
+# start training
+model.compile(loss=losses, loss_weights=loss_weights, optimizer=multisgd, metrics=["accuracy"])
+
+model.fit_generator(train_di,
+                    steps_per_epoch=train_samples // batch_size,
+                    epochs=max_iter,
+                    callbacks=callbacks_list,
+                    validation_data=val_di,
+                    validation_steps=val_samples // batch_size,
+                    use_multiprocessing=False,
+                    initial_epoch=last_epoch
+                    )
+
