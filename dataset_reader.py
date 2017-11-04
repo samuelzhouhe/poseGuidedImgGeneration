19393aed4eca63c05615fa3026653ae5cf183a09
Samuel He <hezhou556@gmail.com>
2017-11-05 01:40:01 +0800

    bug fix for training

--- dataset_reader.py
+++ dataset_reader.py
@@ -119,7 +119,7 @@ class DataLoader:
             target_image[i, :, :, :] = self.images[indexof_targetimg, :, :, :]
             target_morphologicals[i, :, :] = self.morphologicals[indexof_targetimg, :, :]
         g1_feed = np.concatenate([conditional_image, target_pose], axis=3)  # the (batch,256,256,21) thing.
-        return g1_feed, conditional_image,target_image, np.expand_dims(target_morphologicals,axis=2)
+        return g1_feed, conditional_image,target_image, np.expand_dims(target_morphologicals,axis=3)
 
 
 
--- train.py
+++ train.py
@@ -7,15 +7,15 @@ from config import cfg
 dataloader = DataLoader()
 model = Pose_GAN()
 g1_loss, g2_loss, d_loss = model.build_loss()
-
+tf.summary.scalar("g1loss", g1_loss)
+tf.summary.scalar("g2loss", g2_loss)
+tf.summary.scalar("dloss", d_loss)
 sess = tf.Session()
-
-
 train_g1 = tf.train.AdamOptimizer(2e-5).minimize(g1_loss)
 train_g2 = tf.train.AdamOptimizer(2e-5).minimize(g2_loss)
 train_d = tf.train.AdamOptimizer(2e-5).minimize(d_loss)
 
-saver = tf.train.Saver()
+saver = tf.train.Saver(max_to_keep=2)
 summary_writer = tf.summary.FileWriter(cfg.LOGDIR, sess.graph)
 sess.run(tf.global_variables_initializer())
 ckpt = tf.train.get_checkpoint_state(cfg.LOGDIR)
@@ -27,6 +27,7 @@ if ckpt and ckpt.model_checkpoint_path:
 print("Setting up summary op...")
 summary_merge = tf.summary.merge_all()
 
+
 # step 1: train g1
 for itr in range(cfg.MAXITERATION):
     g1_feed, conditional_image, target_image, target_morphologicals = dataloader.next_batch(cfg.BATCH_SIZE)
@@ -36,10 +37,10 @@ for itr in range(cfg.MAXITERATION):
     if itr %5 == 0:
         train_loss, summaryString = sess.run([g1_loss,summary_merge],feed_dict=feed_dict)
         summary_writer.add_summary(summaryString,itr)
-        print("training loss train_loss")
+        print("training loss is", train_loss, "itr",itr)
 
-    if itr == cfg.MAXITERATION - 1 or itr == cfg.MAXITERATION // 2:
-        saver.save(sess, cfg.BATCH_SIZE + "model.ckpt", itr)
+    if itr == cfg.MAXITERATION - 1 or itr%50==0:
+        saver.save(sess, cfg.LOGDIR + "/model.ckpt", global_step=itr)
 
 # step 2: train g2 and d
 for itr in range(cfg.MAXITERATION, 2*cfg.MAXITERATION):
@@ -47,5 +48,5 @@ for itr in range(cfg.MAXITERATION, 2*cfg.MAXITERATION):
     feed_dict = {model.g1_input: g1_feed, model.ia_input:conditional_image,
                  model.ib_input:conditional_image, model.mb_plus_1:target_morphologicals}
     sess.run([train_g2,train_d], feed_dict=feed_dict)
-    if itr == cfg.MAXITERATION - 1 or itr == cfg.MAXITERATION // 2:
-        saver.save(sess, cfg.BATCH_SIZE + "model.ckpt", itr)
+    if itr == cfg.MAXITERATION - 1 or itr %50==0:
+        saver.save(sess, cfg.LOGDIR + "/model.ckpt", global_step=itr)
