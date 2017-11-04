a55d21d4a4038f8b801befc1080b33d45249dfa9
Samuel He <zhead@connect.ust.hk>
2017-11-04 20:45:48 +0800

    dataset reader wip

--- dataset_reader.py
+++ dataset_reader.py
@@ -5,11 +5,15 @@ from os.path import isfile, join
 import cv2
 import scipy
 import itertools
+import pickle
+import matplotlib.pyplot as plt
 
-ROOTDIR = "../clothes/Img/img/WOMEN/Denim"
+ROOTDIR = "./Img_minibatch/img/MEN/Denim"
 
 class DataLoader:
-    images = None
+    images = None #(?,256,256,3)
+    heatmaps = None #(?,256,256,18)
+    morphologicals = None #(?,256,256)
     pairs=[]
     groupsofIndices = []
 
@@ -30,13 +34,38 @@ class DataLoader:
                 if not "flat" in file:
                     img = cv2.imread(fulldir)
                     if img is not None:
+                        # perform left-right flip
                         img = np.expand_dims(img,axis=0)
                         flippedImg = np.flip(img, axis=2)
+                        # process the keypoint thing
+                        heatmap = np.zeros([256,256,18]) # (of original image)
+                        mapofAllPoints = np.zeros([256,256])
+
+                        # process the stored keypoints
+                        with open (fulldir+'keypoints','rb') as kpfile:
+                            keypoints = pickle.load(kpfile)
+                            for i in range(len(keypoints)):
+                                keypoint = keypoints[i]
+                                if len(keypoint)!=0:    # a non-empty keypoint is a list consists of one and only one tuple.
+                                    heatmap[:,:,i] = cv2.circle(np.zeros([256,256]), (keypoint[0][0], keypoint[0][1]), 4,255, -1)
+                                    pass
+                        heatmap = np.expand_dims(heatmap, axis=0)
+                        heatmap_flippedimg = np.flip(heatmap, axis=2)
+
+
+
+
+
+
+                        # add both images and heatmaps to the their respective grand ndarrays
                         if self.images is None:
                             self.images = np.concatenate([img, flippedImg],axis=0)
-                            pass
+                            self.heatmaps = np.concatenate([heatmap, heatmap_flippedimg],axis=0)
                         else:
                             self.images = np.concatenate([self.images,img,flippedImg],axis=0)
+                            self.heatmaps = np.concatenate([self.heatmaps,heatmap,heatmap_flippedimg],axis=0)
+                            pass
+                        # code means "which variation of this cloth". Only clothes with the same code are deemed a PAIR.
                         code = file[0:2]
                         if code in code2index:
                             code2index[code].append(len(self.images)-2)
@@ -44,12 +73,18 @@ class DataLoader:
                         else:
                             code2index[code] = [len(self.images)-2,len(self.images)-1]
                         pass
-            for k,v in code2index.items():
-                self.groupsofIndices.append(v)
+
+
+
+        for k,v in code2index.items():
+            self.groupsofIndices.append(v)
+
+        # Generate pairs
         for group in self.groupsofIndices:
             self.pairs.append(list(itertools.combinations(group,2)))
         self.pairs = list(itertools.chain.from_iterable(self.pairs))
-        print(len(self.pairs))
+
+        print(self.pairs)
         pass
 
     def next_batch(self, batch_size):
