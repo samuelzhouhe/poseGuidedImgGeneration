8e9053523eee94b67e616964cbe46c5c9809da75
Coeusguo <coeusguosp@gmail.com>
2017-11-21 16:14:47 +0800

    bug fixed

--- trainall.py
+++ trainall.py
@@ -8,6 +8,10 @@ import datetime
 import numpy as np
 import scipy.misc
 
+def transform(img):
+    img = (img + 1) / 2.0
+    return img[:, :, [2, 1, 0]]
+
 dataloader = DataLoader()
 model = Pose_GAN()
 g1_loss, g2_loss, d_loss= model.build_loss()
@@ -59,21 +63,22 @@ if (start_itr < cfg.MAXITERATION):
         if itr % 1000 == 0:
             final_output, g2_out, g1_out = sess.run([model.final_output, model.g2_output, model.g1_output],
                                                     feed_dict=feed_dict)
+
             size = final_output.shape[0]
             dir_name = cfg.RESULT_DIR + '/g1_iter_' + str(itr) + 'at' + str(datetime.datetime.now())
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
             for i in range(size):
                 name = dir_name + '/sample' + str(i + 1) + 'finalout.jpg'
-                cv2.imwrite(name, final_output[i])
+                scipy.misc.imsave(name, transform(final_output[i]))
                 name = dir_name + '/sample' + str(i + 1) + 'g2out.jpg'
-                cv2.imwrite(name, g2_out[i])
+                scipy.misc.imsave(name, transform(g2_out[i]))
                 name = dir_name + '/sample' + str(i + 1) + 'g1out.jpg'
-                cv2.imwrite(name, g1_out[i])
+                scipy.misc.imsave(name, transform(g1_out[i]))
                 name_cond = dir_name + '/sample' + str(i + 1) + 'conditionalimg.jpg'
-                cv2.imwrite(name_cond, conditional_image[i, :, :, :])
+                scipy.misc.imsave(name_cond, transform(conditional_image[i, :, :, :]))
                 name_target = dir_name + '/sample' + str(i + 1) + 'target.jpg'
-                cv2.imwrite(name_target, target_image[i, :, :, :])
+                scipy.misc.imsave(name_target, transform(target_image[i, :, :, :]))
 
             g1_feed, conditional_image, target_image, target_morphologicals = dataloader.next_batch(cfg.BATCH_SIZE,
                                                                                                     trainorval='VALIDATION')
@@ -82,8 +87,7 @@ if (start_itr < cfg.MAXITERATION):
             val_g1loss = sess.run(g1_loss,feed_dict=feed_dict)
             print("Validation G1 loss at itr ", itr, " is ", val_g1loss)
 
-def transform(img):
-    return (img + 1) / 2.0
+
 
 
 saver = tf.train.Saver(max_to_keep=2)
@@ -92,7 +96,7 @@ for itr in range(cfg.MAXITERATION-1, 4*cfg.MAXITERATION):
     g1_feed, conditional_image, target_image, target_morphologicals = dataloader.next_batch(cfg.BATCH_SIZE_G2D, trainorval='TRAIN')
     feed_dict = {model.g1_input: g1_feed, model.ia_input:conditional_image,
                  model.ib_input: target_image, model.mb_plus_1:target_morphologicals}
-    sess.run([train_g2], feed_dict=feed_dict)
+                 
     sess.run([train_g2], feed_dict=feed_dict)
 
     sess.run([train_d], feed_dict=feed_dict)
@@ -112,21 +116,21 @@ for itr in range(cfg.MAXITERATION-1, 4*cfg.MAXITERATION):
     if itr % 100 == 0:
         final_output, g2_out, g1_out = sess.run([model.final_output, model.g2_output, model.g1_output], feed_dict=feed_dict)
         size = final_output.shape[0]
-        final_output = transform(final_output)
+
         dir_name = cfg.RESULT_DIR + '/g2_iter_' + str(itr) + 'at' + str(datetime.datetime.now())
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
         for i in range(size):
             name = dir_name + '/sample' + str(i + 1) + 'finalout.jpg'
-            scipy.misc.imsave(name, final_output[i])
+            scipy.misc.imsave(name, transform(final_output[i]))
             name = dir_name + '/sample' + str(i + 1) + 'g2out.jpg'
-            scipy.misc.imsave(name, g2_out[i])
+            scipy.misc.imsave(name, transform(g2_out[i]))
             name = dir_name + '/sample' + str(i + 1) + 'g1out.jpg'
-            scipy.misc.imsave(name, g1_out[i])
+            scipy.misc.imsave(name, transform(g1_out[i]))
             name_cond = dir_name + '/sample' + str(i + 1) + 'conditionalimg.jpg'
-            scipy.misc.imsave(name_cond, conditional_image[i, :, :, :])
+            scipy.misc.imsave(name_cond, transform(conditional_image[i, :, :, :]))
             name_target = dir_name + '/sample' + str(i + 1) + 'target.jpg'
-            scipy.misc.imsave(name_target, target_image[i, :, :, :])
+            scipy.misc.imsave(name_target, transform(target_image[i, :, :, :]))
 
         g1_feed, conditional_image, target_image, target_morphologicals = dataloader.next_batch(cfg.BATCH_SIZE,
                                                                                                 trainorval='VALIDATION')
